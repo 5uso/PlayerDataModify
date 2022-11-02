120adcc40e5633696d7c4a6f3cb8139437dc7703
Suso <jesusmosteiro@gmail.com>
2022-11-02 12:13:19 +0100

    Initial commit

---
+++ src/main/java/suso/player_data_modify/Player_data_modify.java
@@ -0,0 +1,10 @@
+package suso.player_data_modify;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Player_data_modify implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Using Suso's player data modify mod.");
+    }
+}
---
+++ src/main/java/suso/player_data_modify/client/Player_data_modifyClient.java
@@ -0,0 +1,13 @@
+package suso.player_data_modify.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class Player_data_modifyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
